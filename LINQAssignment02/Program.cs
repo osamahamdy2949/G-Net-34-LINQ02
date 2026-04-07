3aefcce4114a2e3198448b098d1c07f5cc7604a7
osamahamdy2949 <osama.hamdy2949@gmail.com>
2026-04-07 14:35:47 +0200

    Question01

--- LINQAssignment02/Program.cs
+++ LINQAssignment02/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace LINQAssignment02
+﻿using static LINQ.DataSources.Source;
+//using static LINQ.DataSources.Source;
+
+namespace LINQAssignment02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
+            #region Question01
+            //var result = ProductList.OrderBy(p => p.UnitPrice).Reverse().Take(3);
+            #endregion
+
+
+
+
+
+
+
+
+
+            foreach (var item in result)
+                Console.WriteLine(item);
         }
     }
 }
